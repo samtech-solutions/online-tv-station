bc1f015a65616b7215c1f2a7827ac14a533ef10d
Sammy Ndung'u <57710668+samtech-solutions@users.noreply.github.com>
2023-12-12 23:13:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+ function myFunction1(){
+    document.getElementById("myframe").src="https://www.youtube.com/embed/NTTNAyl7Q8s?autoplay=1";
+ }
+
+ function myFunction2(){
+    document.getElementById("myframe").src="https://www.youtube.com/embed/Ri0dsNRnzVU?autoplay=1";
+ }
+
+ function myFunction3(){
+    document.getElementById("myframe").src="https://www.youtube.com/embed/A2XaDFyi2_A?autoplay=1";
+ }
+
+ function myFunction4(){
+    document.getElementById("myframe").src="https://www.youtube.com/embed/9F8EL7W8yzE?autoplay=1";
+ }
+ function myFunction5(){
+    document.getElementById("myframe").src="https://www.youtube.com/embed/Tk6NmkeyW6w?autoplay=1";
+ }
+ function myFunction6(){
+    document.getElementById("myframe").src="https://zeno.fm/radio/hustler-radio/";
+ }
+ function myFunction7(){
+    document.getElementById("myframe").src="https://www.youtube.com/embed/7dWJqAtlLxU";
+ }
